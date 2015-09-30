70595a5541287366bc5de03caea2f8b074b07564
Geordie J <geojay@gmail.com>
2015-09-30 15:07:00 +0200

    Publish 0.1.0

--- package.js
+++ package.js
@@ -2,12 +2,12 @@ Package.describe({
 	name: 'ephemer:microevent',
     summary: 'A simple DOMless events bus (less than 0.5kb)',
     version: '0.1.0',
-    git: ''
+    git: 'https://github.com/ephemer/microevent.js.git'
 });
 
 Package.onUse(function(api) {
     api.versionsFrom('1.0');
-    api.use(['grigio:babel']); // remove this in version 1.0 for Meteor 1.2+
+    api.use(['grigio:babel@0.1.8']); // remove this in version 1.0 for Meteor 1.2+
     api.add_files('microevent.es6.js');
     api.export('MicroEvent');
 });
